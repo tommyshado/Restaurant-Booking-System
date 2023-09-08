0025e490def0634750d25d50848f10f104938805
zukelwa20 <makhosandile@projectcodex.co>
2023-09-08 17:56:14 +0200

    added some test cases and fix some tests

--- test/restaurant.test.js
+++ test/restaurant.test.js
@@ -1,5 +1,5 @@
 import assert from "assert"
-import Resturent_booking_table  from "../service/rastaurant.js";
+import resturent_booking_table  from "../service/rastaurant.js";
 import pgPromise from 'pg-promise';
 
 const connectionString = process.env.DATABASE_URL || dbstring
@@ -19,78 +19,84 @@ describe("The restaurant booking table", function () {
     });
 
     it("It should check if the status changes after the booking.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+        let Resturent_booking_table = resturent_booking_table(db);
+        
+      
+        assert.deepEqual(true, false);
+    });
+
+    it("", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
     it("It should check if the booking has a valid name.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, true);
     });
 
     it(" It should check if the capacity is greater than the available seats.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
-    it("Check if there are available seats for a booking.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should check if there are available seats for a booking.", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
-      
         assert.deepEqual(true, false);
     });
 
     it("Check if the booking has a user name provided.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
-    it("Create a test case to attempt booking a table with an invalid table name.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should attempt booking a table with an invalid table name.", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
-    it("Create a test case to retrieve a list of all tables and their booking statuses.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should list of all the tables and their booking statuses.", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
-    it("Create a test case to retrieve a list of available tables.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should list all the available bookings", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, fasle);
     });
 
-    it("It should list all booked tables.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should list all booked tables.", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
     
-    it("It should list all booked tables.", async function () {
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should list all booked tables.", async function () {
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
     });
 
-    it("it should test if there are correct number of people booking", async function (){
-        let resturent_booking_table = Resturent_booking_table(db);
+    it("should test if there are correct number of people booking", async function (){
+        let Resturent_booking_table = resturent_booking_table(db);
         
       
         assert.deepEqual(true, false);
